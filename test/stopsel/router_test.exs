223053883fc83691b7363f2456f0db8de1a536f7
Awlex <no thank you>
2021-03-11 22:46:52 +0100

    Not a thing anymore

--- test/stopsel/router_test.exs
+++ test/stopsel/router_test.exs
@@ -8,14 +8,4 @@ defmodule Stopsel.RouterTest do
     Stopsel.Router.load_route(MyApp.Router, ~w"hello")
     {:ok, _} = Stopsel.Router.match_route(MyApp.Router, ~w"hello")
   end
-
-  test "returns error when multiple matches where found" do
-    Stopsel.Router.unload_router(MyApp.Router)
-
-    :router.new(MyApp.Router)
-    :router.add(MyApp.Router, ~w"hello", {:a, :b, :c})
-    :router.add(MyApp.Router, ~w"hello", {:a, :b, :d})
-
-    {:error, {:multiple_matches, _}} = Stopsel.Router.match_route(MyApp.Router, ~w"hello")
-  end
 end
